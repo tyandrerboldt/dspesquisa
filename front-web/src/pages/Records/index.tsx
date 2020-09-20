8d1c2bac1539ed69d5c44f294ca3a9aed7fec6f9
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-09-20 18:33:30 -0300

    Configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -7,7 +7,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://sds1-tyandrer.herokuapp.com"
 
 const Records = () => {
 
